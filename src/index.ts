d1091972e04c1a3a54e4cd123615e4ee27dffd99
knightlore <gnightshade@gmail.com>
2025-07-14 16:01:43 -0300

    Team id validation

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import morgan from 'morgan'
 import { agents } from './agents'
 import { supabase } from './agents/db'
 
-const version = '1.0709.1308'
+const version = '1.0714.1601'
 
 const app = express()
 app.use(cors())
@@ -18,17 +18,31 @@ app.get('/', (req, res) => {
 })
 
 app.post('/agent', async (req: Request<any, any, AgentParameters & { id: number, team_id?: string }>, res) => {
+
   let input = req.body
+
   try {
+
+    if (!input.team_id) {
+      res.status(400).send("team_id is required")
+      return
+    }
+
+    const team = await supabase.from("teams").select("*").eq("id", input.team_id).single()
+    if (!team.data || team.error) {
+      res.status(400).send("team_id is not valid")
+      return
+    }
+
     const a = await agents.loadAgent(input.id)
-    
+
     let apiKeys = {}
     if (input.team_id) {
       const { data, error } = await supabase
         .from("api_keys")
         .select("*")
         .eq("team_id", input.team_id)
-      
+
       if (error) {
         console.error('Error fetching api keys:', error)
       } else {
